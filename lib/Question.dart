cdb77de61169fd4df76efc44cefb7ef3f406f28b
Hossein <Rahmatei@gmail.com>
2021-12-15 01:26:11 +0330

    Add_AnswerAndQuestionWidget

---
+++ lib/Question.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Question extends StatelessWidget {
+  final String question;
+
+  Question({required this.question});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      question,
+      style: TextStyle(
+        fontSize: 20,
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+}
